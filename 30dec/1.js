8423b4f0bf0ad60ffb444f5daec98663c4c510f5
unknown <omkarrkasar1998@gmail.com>
2023-01-03 23:15:45 +0530

    second commit

---
+++ 30dec/1.js
@@ -0,0 +1,12 @@
+let details={
+  name: "omkar",
+ age: 24,
+adress: "adgoan",
+phone:4646,
+father_name:"ramdas",
+mother_name:"usha",
+};
+console.log(details);
+details.hobbie="judo";
+details["car"]="audi";
+console.log(details);
